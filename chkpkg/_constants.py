252da4fb52fa1ebefa8b67748419b87b75ccd780
rtmigo <art.galkin@gmail.com>
2021-05-17 19:56:04 +0300

    publish

--- chkpkg/_constants.py
+++ chkpkg/_constants.py
@@ -1,7 +1,7 @@
 __name__ = "chkpkg",
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = "Artёm IG"
 __author_email__ = "ortemeo@gmail.com"
 __license__ = "MIT"
-__summary__ = "Checks Python packages intended to be published on PyPi",
+__summary__ = "Checks Python packages intended to be published on PyPi"
 __keywords__ = "package distribution wheel whl testing".split()
