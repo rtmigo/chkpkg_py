e8fe6524f46a291f001de969500a30ec69414541
rtmigo <art.galkin@gmail.com>
2021-05-17 06:53:51 +0300

    publish

--- chkpkg/_constants.py
+++ chkpkg/_constants.py
@@ -3,7 +3,7 @@ __version__ = '0.0.2'
 __author__ = "Artёm IG"
 __author_email__ = "ortemeo@gmail.com"
 __license__ = "MIT"
-__url__ = "https://github.com/rtmigo/chkpkg_py#readme",
+__url__ = "https://github.com/rtmigo/chkpkg_py",
 __summary__ = "Checks Python packages intended to be published on PyPi",
 __keywords__ = "package distribution wheel whl testing".split()
 
