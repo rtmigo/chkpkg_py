b23bd9318b25a671d467f5317d56e6649891ed60
rtmigo <art.galkin@gmail.com>
2021-05-18 22:34:30 +0300

    py testing

--- test_unit.py
+++ test_unit.py
@@ -3,16 +3,18 @@ from pathlib import Path
 from subprocess import check_call
 
 parent = Path(__file__).parent
-tsts = parent / "test_projects"
+tests = parent / "test_projects"
 
 
-def divide():
+def splitter():
     print('\n' + '\n'.join('/' * 80 for _ in range(3)) + '\n')
 
 
 check_call([sys.executable, '-m', 'pip', 'install', '-e', '.'], cwd=parent)
-divide()
-check_call([sys.executable, 'test_pkg.py'], cwd=tsts / 'greeter')
-divide()
-check_call([sys.executable, 'test_pkg.py'], cwd=tsts / 'invalid_metadata')
+splitter()
+
+check_call([sys.executable, 'test_pkg.py'], cwd=tests/'greeter')
+splitter()
+
+check_call([sys.executable, 'test_pkg.py'], cwd=tests/'invalid_metadata')
 
