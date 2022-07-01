8b7e23f4c66a1550ae8250c661fbdb403778558a
Artem IG <ortemeo@gmail.com>
2022-07-01 22:07:37 +0300

    require_pytyped

--- tests/test_getmodulepath.py
+++ tests/test_getmodulepath.py
@@ -6,7 +6,7 @@ from chkpkg._require_pytyped import get_module_path
 
 class TestGetModulePath(unittest.TestCase):
     def test(self):
-        p = get_module_path(sys.executable, 'urllib3')
+        p = get_module_path(sys.executable, 'pip')
 
         # .../site-packages/urllib3/__init__.py
         self.assertTrue(p.exists())
